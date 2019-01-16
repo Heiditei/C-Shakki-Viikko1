4fba721251b84024c03688c59576583e63a61c70
Heiditei <heidi.teinonen@metropolia.fi>
2019-01-16 14:28:55 +0200

    Create Station.h

---
+++ Station.h
@@ -0,0 +1,26 @@
+#pragma once
+class Station
+{
+public:
+	//Konstruktori luo alkuaseman laudalle
+	Station();
+	Piece* board[8][8];
+}
+
+
+};
+/*
+VK, L"\u2654"	//White King
+VD, L"\u2655"	//White Queen
+VT, L"\u2656"	//White Tower	 
+VL, L"\u2657"	//White Bishop
+VR, L"\u2658"	//White Knight
+VS, L"\u2659"	//White Soldier
+
+MK, L"\u265A"	//Black King
+MD, L"\u265B"	//Black Queen
+MT, L"\u265C"	//Black Tower
+ML, L"\u265D"	//Black Bishop
+MR, L"\u265E"	//Black Knight
+MS, L"\u265F"	//Black Soldier
+*/
