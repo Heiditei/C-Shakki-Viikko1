1f5a177ca883ab52984fc0ccc4ce9d16cbef752d
Heiditei <heidi.teinonen@metropolia.fi>
2019-01-16 14:33:53 +0200

    Update Station.h

--- Station.h
+++ Station.h
@@ -15,12 +15,12 @@ VD, L"\u2655"	//White Queen
 VT, L"\u2656"	//White Tower	 
 VL, L"\u2657"	//White Bishop
 VR, L"\u2658"	//White Knight
-VS, L"\u2659"	//White Soldier
+VS, L"\u2659"	//White Pawn
 
 MK, L"\u265A"	//Black King
 MD, L"\u265B"	//Black Queen
 MT, L"\u265C"	//Black Tower
 ML, L"\u265D"	//Black Bishop
 MR, L"\u265E"	//Black Knight
-MS, L"\u265F"	//Black Soldier
+MS, L"\u265F"	//Black Pawn
 */
